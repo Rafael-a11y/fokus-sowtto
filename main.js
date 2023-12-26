4e793e27c8180a5011f4244d5624ad30994c2291
Rafael Souto da Silva <rsowtto@gmail.com>
2023-12-26 19:20:52 -0300

    Commit teste

--- main.js
+++ main.js
@@ -1,17 +1,18 @@
 const html = document.querySelector("html");
-const focoBotao = document.querySelector(".app__card-button--foco");
-const curtoBotao = document.querySelector(".app__card-button--curto");
-const longoBotao = document.querySelector(".app__card-button--longo");
+const botaoFoco = document.querySelector(".app__card-button--foco");
+const botaoCurto = document.querySelector(".app__card-button--curto");
+const botaoLongo = document.querySelector(".app__card-button--longo");
 
-focoBotao.addEventListener("click", ()=>
+botaoFoco.addEventListener("click", ()=>
 {
     html.setAttribute("data-contexto", "foco");
 });
-curtoBotao.addEventListener("click", ()=>
+botaoCurto.addEventListener("click", ()=>
 {
     html.setAttribute("data-contexto", "descanso-curto");
 });
-longoBotao.addEventListener("click", ()=>
+botaoLongo.addEventListener("click", ()=>
 {
     html.setAttribute("data-contexto", "descanso-longo");
 });
+
