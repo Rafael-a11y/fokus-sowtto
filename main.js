a22ee424aa2073bbaecdf17cc779e40807ea6351
Rafael Souto da Silva <rsowtto@gmail.com>
2024-01-03 16:12:24 -0300

    Projeto temporariamente concluído na branch do professor

--- main.js
+++ main.js
@@ -6,9 +6,35 @@ const banner = document.querySelector(".app__image");
 const titulo = document.querySelector(".app__title");
 const botoes = document.querySelectorAll(".app__card-button");
 const inputMusica = document.querySelector("#alternar-musica");
+const startPauseBt = document.querySelector("#start-pause");
+const startPauseIcon = document.querySelector("#start-pause>img");
+const spanIniciarPausar = document.querySelector("#start-pause>span");
+const tempoNaTela = document.querySelector("#timer");
+// const bt = document.getElementById("start-pause");
+// console.log(bt);
+// const musicaAmbiente = document.createElement("audio");
+// musicaAmbiente.setAttribute("src", "./sons/luna-rise-part-one.mp3");
 const musicaAmbiente = new Audio("./sons/luna-rise-part-one.mp3");
+const audioStart = new Audio("./sons/play.wav");
+const audioPause = new Audio("./sons/pause.mp3");
+const audioAlarme = new Audio("./sons/beep.mp3");
 musicaAmbiente.loop = true;
+let tempoDecorridoEmSegundos = 1500;
+let intervaloId = null;
 
+const contagemRegressiva = () =>
+{
+    if(tempoDecorridoEmSegundos <= 0)
+    {
+        // clearInterval(intervaloId);
+        zerar();
+        audioAlarme.play();
+        alert("temporizador finalizado");
+        return;
+    }
+    tempoDecorridoEmSegundos--;
+    mostrarTempo();
+};
 
 inputMusica.addEventListener("change", () =>
 {
@@ -18,25 +44,28 @@ inputMusica.addEventListener("change", () =>
 
 botaoFoco.addEventListener("click", ()=>
 {
+    tempoDecorridoEmSegundos = 1500;
     alterarContexto("foco");
     botaoFoco.classList.add("active");
 });
 botaoCurto.addEventListener("click", ()=>
 {
+    tempoDecorridoEmSegundos = 300;
     alterarContexto("descanso-curto");
     botaoCurto.classList.add("active");
 });
 botaoLongo.addEventListener("click", ()=>
 {
+    tempoDecorridoEmSegundos = 900;
     alterarContexto("descanso-longo");
     botaoLongo.classList.add("active");
 });
 
 function alterarContexto(contexto)
 {
+    mostrarTempo();
     botoes.forEach((botao) => 
     {
-        console.log(botoes.length);
         botao.classList.remove("active");
     });
     html.setAttribute("data-contexto", contexto);
@@ -55,9 +84,40 @@ function alterarContexto(contexto)
         "<strong class=app__title-strong>Faça uma pausa longa.</strong>";
         break;
     }
-    const result = true;
-
-    titulo.textContent = `'c' ${result ? "added" : "removed"}; classList is now "${titulo.classList}".`;
 }
 
+startPauseBt.addEventListener("click", iniciarOuPausar);
 
+
+function zerar()
+{
+    clearInterval(intervaloId);
+    startPauseIcon.src = "./imagens/play_arrow.png";
+    spanIniciarPausar.textContent = "Começar";
+    intervaloId = null;
+}
+
+function iniciarOuPausar()
+{
+    if(intervaloId)
+    {
+        console.log("valor: " + intervaloId);
+        audioPause.play();
+        zerar();
+        return;
+    }
+    if(!audioAlarme.paused) audioAlarme.pause();
+    startPauseIcon.src = "./imagens/pause.png";
+    spanIniciarPausar.textContent = "Pausar";
+    audioStart.play();
+    intervaloId = setInterval(contagemRegressiva, 1000);
+}
+
+function mostrarTempo()
+{
+    const tempo = new Date(tempoDecorridoEmSegundos * 1000);
+    const tempoFormatado = tempo.toLocaleTimeString("pt-br", {minute: "2-digit", second: "2-digit"});
+    tempoNaTela.innerHTML = `${tempoFormatado}`;
+}
+
+mostrarTempo();
---
+++ mainNode.js
@@ -0,0 +1,21 @@
+const dateString = "2024/01/03";
+const currentDate = new Date(dateString);
+const ano = currentDate.getFullYear();  // Acessa o ano
+const mes = currentDate.getMonth(); // Acessa o mês - Janeiro é 0, Fevereiro é 1, ..., Dezembro é 11
+const dia = currentDate.getDate(); // Acessa o dia
+const horas = currentDate.getHours(); // Acessa as horas 
+const minutos = currentDate.getMinutes(); // Acessa os minutos
+const segundos = currentDate.getSeconds(); // Acessa os segundos
+const milissegundos = currentDate.getMilliseconds();  // Acessa os milissegundos 
+console.clear();
+
+const data = new Date();
+
+data.setFullYear(2024);  // Define o ano
+data.setMonth(10); // Define o mês
+data.setDate(25); // Define o dia
+data.setHours(10);  // Define as horas
+data.setMinutes(30); // Define os minutos
+data.setSeconds(0); // Define os segundos
+
+console.log(data.getMonth().toLocaleString());
